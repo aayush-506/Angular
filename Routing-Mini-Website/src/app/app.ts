3d06d00fe9cb8d1cd6e577336bc7382c2aa0800c
Aayush Chauhan <at290608@gmail.com>
2025-08-23 00:32:26 +0530

    adding UI to home , about , contact

--- Routing-Mini-Website/src/app/app.ts
+++ Routing-Mini-Website/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { About } from './about/about';
 import { Home } from './home/home';
 import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet,About,Home,Contact],
+  imports: [RouterLink,RouterLinkActive, RouterOutlet,About,Home,Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
