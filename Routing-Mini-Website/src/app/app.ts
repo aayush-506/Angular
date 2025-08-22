12f6cb30972ad9a1bc809952210be79bc3d71ecd
Aayush Chauhan <at290608@gmail.com>
2025-08-22 23:32:50 +0530

    creating navbar and adding routes

--- Routing-Mini-Website/src/app/app.ts
+++ Routing-Mini-Website/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { About } from './about/about';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet,About,Home,Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
