1b75bf0a1cda2a18833418d82dc6aa478223c6e2
Aayush Chauhan <at290608@gmail.com>
2025-09-10 18:54:18 +0530

    sidebar show and hide feature added

--- Student-Dashboard/src/app/appModules/mat-module.ts
+++ Student-Dashboard/src/app/appModules/mat-module.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 const modules = [
@@ -15,6 +16,7 @@ const modules = [
    MatButtonModule,
    MatSidenavModule,
    MatListModule,
+   MatToolbarModule
 ];
 
 
--- Student-Dashboard/src/app/pages/main-dashboard/main-dashboard.ts
+++ Student-Dashboard/src/app/pages/main-dashboard/main-dashboard.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
 import { Sidenav } from '../../components/sidenav/sidenav';
+import { Navbar } from '../../components/navbar/navbar';
+import { DashboardData } from '../../service/dashboard-data';
 
 @Component({
   selector: 'app-main-dashboard',
-  imports: [MatModule,Sidenav],
+  imports: [MatModule,Sidenav,Navbar],
   templateUrl: './main-dashboard.html',
   styleUrl: './main-dashboard.scss'
 })
 export class MainDashboard {
+
+   isOpened = false;
+  constructor(private dashboardData : DashboardData){}
+
+  ngOnInit(){
+    this.dashboardData.sidenavState.subscribe({
+      next : result => this.isOpened = result 
+    })
+  }
 
 }
