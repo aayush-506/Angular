1c979b448181e512bc19be1dbe15395547b661ea
Aayush Chauhan <at290608@gmail.com>
2025-09-14 18:57:41 +0530

    passes the small-cards specific data

--- Student-Dashboard/src/app/pages/main-dashboard/main-dashboard.ts
+++ Student-Dashboard/src/app/pages/main-dashboard/main-dashboard.ts
@@ -10,13 +10,19 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-dashboard',
-  imports: [CommonModule, MatModule,Sidenav,Navbar,SmallCardDashboard,GreetingHeading,LargeCardDashboard],
+  imports: [CommonModule,MatModule,Sidenav,Navbar,SmallCardDashboard,GreetingHeading,LargeCardDashboard],
   templateUrl: './main-dashboard.html',
   styleUrl: './main-dashboard.scss'
 })
 export class MainDashboard {
 
    isOpened = false;
+    smallCards = [
+    {title:"Current GPA",icon:"school",iconColor:"#00A63E",iconBg:"#F0FDF4"},
+    {title:"Credits Completed",icon:"menu_book",iconColor:"#155DFC",iconBg:"#EFF6FF"},
+    {title:"Pending Assignments",icon:"assignment_turned_in",iconColor:"#F54A00",iconBg:"#FFF7ED"},
+    {title:"Current Attendance",icon:"calendar_month",iconColor:"#9810FA",iconBg:"#FAF5FF"}
+   ];
   constructor(private dashboardData : DashboardData){}
 
    ngOnInit(){
