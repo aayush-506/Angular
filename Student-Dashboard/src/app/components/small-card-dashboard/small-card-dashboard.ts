28884d7a518d7e5ec42ea894b30f529b938a5e31
Aayush Chauhan <at290608@gmail.com>
2025-09-14 18:56:35 +0530

    added specific icon and icon-background-color to each specific card

--- Student-Dashboard/src/app/components/small-card-dashboard/small-card-dashboard.ts
+++ Student-Dashboard/src/app/components/small-card-dashboard/small-card-dashboard.ts
@@ -1,13 +1,19 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-small-card-dashboard',
-  imports: [MatModule],
+  imports: [MatModule,CommonModule],
   templateUrl: './small-card-dashboard.html',
   styleUrl: './small-card-dashboard.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SmallCardDashboard {
-
+@Input() card!: {       
+    title: string;
+    icon: string;
+    iconColor: string;
+    iconBg: string;
+  };
 }
