185d5d0f6d6286064b27fe01897d3c881b9665e9
Aayush Chauhan <at290608@gmail.com>
2025-09-15 12:14:11 +0530

    added Assignments and Grades cards

--- Student-Dashboard/src/app/components/large-card-dashboard/large-card-dashboard.ts
+++ Student-Dashboard/src/app/components/large-card-dashboard/large-card-dashboard.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
 import { AssignmentBarDashboard } from '../assignment-bar-dashboard/assignment-bar-dashboard';
 import { GradeBarDashboard } from '../grade-bar-dashboard/grade-bar-dashboard';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-large-card-dashboard',
-  imports: [MatModule,AssignmentBarDashboard,GradeBarDashboard],
+  imports: [CommonModule,MatModule,AssignmentBarDashboard,GradeBarDashboard],
   templateUrl: './large-card-dashboard.html',
   styleUrl: './large-card-dashboard.scss'
 })
 export class LargeCardDashboard {
-
+@Input() largeCard! : {
+  title : string ;
+};
 }
