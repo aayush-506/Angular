cb4221efb17dde57a06903566706981b789f53f0
Aayush Chauhan <at290608@gmail.com>
2025-09-15 14:58:48 +0530

    added routes

--- Student-Dashboard/src/app/app.routes.ts
+++ Student-Dashboard/src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
+import { Courses } from './pages/courses/courses';
+import { MainDashboard } from './pages/main-dashboard/main-dashboard';
+import { Attendence } from './pages/attendence/attendence';
+import { Grades } from './pages/grades/grades';
+import { Assignments } from './pages/assignments/assignments';
 
 
 export const routes: Routes = [
-
+{path:"" , component:MainDashboard},
+{path:"courses" , component:Courses},
+{path:"attendance" , component:Attendence},
+{path:"grades" , component:Grades},
+{path:"assignments" , component:Assignments}
 ];
