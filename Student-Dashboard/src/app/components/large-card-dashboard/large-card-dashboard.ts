354965963493ac7d57938034e11d923df036e2c9
Aayush Chauhan <at290608@gmail.com>
2025-09-15 15:26:17 +0530

    added routing to view btn

--- Student-Dashboard/src/app/components/large-card-dashboard/large-card-dashboard.ts
+++ Student-Dashboard/src/app/components/large-card-dashboard/large-card-dashboard.ts
@@ -3,10 +3,11 @@ import { MatModule } from '../../appModules/mat-module';
 import { AssignmentBarDashboard } from '../assignment-bar-dashboard/assignment-bar-dashboard';
 import { GradeBarDashboard } from '../grade-bar-dashboard/grade-bar-dashboard';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-large-card-dashboard',
-  imports: [CommonModule,MatModule,AssignmentBarDashboard,GradeBarDashboard],
+  imports: [RouterLink,CommonModule,MatModule,AssignmentBarDashboard,GradeBarDashboard],
   templateUrl: './large-card-dashboard.html',
   styleUrl: './large-card-dashboard.scss'
 })
