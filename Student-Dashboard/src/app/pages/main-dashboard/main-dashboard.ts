a2800a9649a8531fde65e9bd334977d7f086d33d
Aayush Chauhan <at290608@gmail.com>
2025-09-15 14:49:04 +0530

    sperated main-dashboard and app component

--- Student-Dashboard/src/app/pages/main-dashboard/main-dashboard.ts
+++ Student-Dashboard/src/app/pages/main-dashboard/main-dashboard.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
 import { Sidenav } from '../../components/sidenav/sidenav';
 import { Navbar } from '../../components/navbar/navbar';
-import { DashboardData } from '../../service/dashboard-data';
 import { SmallCardDashboard } from '../../components/small-card-dashboard/small-card-dashboard';
 import { GreetingHeading } from '../../components/greeting-heading/greeting-heading';
 import { LargeCardDashboard } from '../../components/large-card-dashboard/large-card-dashboard';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-dashboard',
-  imports: [CommonModule,MatModule,Sidenav,Navbar,SmallCardDashboard,GreetingHeading,LargeCardDashboard],
+  imports: [RouterOutlet,CommonModule,MatModule,Sidenav,Navbar,SmallCardDashboard,GreetingHeading,LargeCardDashboard],
   templateUrl: './main-dashboard.html',
   styleUrl: './main-dashboard.scss'
 })
@@ -26,17 +26,6 @@ export class MainDashboard {
     {title : "Upcoming Assignments"},
     {title : "Recent Grades"}
   ]
-  isOpened = false;
-  constructor(private dashboardData : DashboardData){}
 
-   ngOnInit(){
-    this.dashboardData.sidenavState.subscribe({
-      next : result => {
-        this.isOpened = result ;
-        console.log("updated value :" ,result);
-        
-      } 
-    })
-  }
 
 }
