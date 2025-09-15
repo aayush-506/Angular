7b50c695a939f420d11ea112b79efb47348cfb4f
Aayush Chauhan <at290608@gmail.com>
2025-09-15 15:25:28 +0530

    added rputing

--- Student-Dashboard/src/app/components/notification-icon/notification-icon.ts
+++ Student-Dashboard/src/app/components/notification-icon/notification-icon.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-notification-icon',
-  imports: [MatModule],
+  imports: [MatModule,RouterLink],
   templateUrl: './notification-icon.html',
   styleUrl: './notification-icon.scss'
 })
