0e9ed912783ad5ddd001d75b1971902310ace4f4
Aayush Chauhan <at290608@gmail.com>
2025-09-17 18:42:51 +0530

    featching data with announcements API

--- Student-Dashboard/src/app/pages/announcements/announcements.ts
+++ Student-Dashboard/src/app/pages/announcements/announcements.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { DashboardData } from '../../service/dashboard-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-announcements',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './announcements.html',
   styleUrl: './announcements.scss'
 })
 export class Announcements {
+
+announcements : any[] = [];
+constructor(private dashboardData : DashboardData , private cdr : ChangeDetectorRef){}
+
+ngOnInit(){
+  this.getAnnouncements();
+}
+
+getAnnouncements(){
+  this.dashboardData.getAnnouncementsData().subscribe((data:any)=>{
+    this.announcements = data ;
+    console.log(this.announcements);
+    this.cdr.detectChanges();
+  })
+}
 
 }
