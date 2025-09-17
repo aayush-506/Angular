4f23b9519e1522c56fd37985a16a71594ce18a69
Aayush Chauhan <at290608@gmail.com>
2025-09-17 23:16:28 +0530

    fetching the length of the announcemnts

--- Student-Dashboard/src/app/components/notification-icon/notification-icon.ts
+++ Student-Dashboard/src/app/components/notification-icon/notification-icon.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
 import { RouterLink } from '@angular/router';
+import { DashboardData } from '../../service/dashboard-data';
+import { map, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-notification-icon',
-  imports: [MatModule,RouterLink],
+  imports: [AsyncPipe,MatModule,RouterLink],
   templateUrl: './notification-icon.html',
   styleUrl: './notification-icon.scss'
 })
 export class NotificationIcon {
 hidden = false;
+announcementsCount! : Observable<number>;
+constructor(private dashboardDataService : DashboardData){}
+
+ngOnInit(){
+  this.announcementsCount = this.dashboardDataService.getAnnouncementsData().pipe(map(data=>data.length));
+    
+}
+
 
   toggleBadgeVisibility() {
     this.hidden = !this.hidden;
   }
+
 }
