8ec48c098afb0fa76b2bd445df67edbc1403dc32
Aayush Chauhan <at290608@gmail.com>
2025-09-17 18:43:24 +0530

    added API for announcements data

--- Student-Dashboard/src/app/service/dashboard-data.ts
+++ Student-Dashboard/src/app/service/dashboard-data.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -8,8 +9,15 @@ export class DashboardData {
   
  sidenavState = new BehaviorSubject<boolean>(false);
 
+constructor(private http : HttpClient){}
+announcementUrl = "http://localhost:3000/announcements";
+
 sidenavToggle(){
   this.sidenavState.next(!this.sidenavState.value);
+}
+
+getAnnouncementsData(){
+  return this.http.get(this.announcementUrl);
 }
 
 }
