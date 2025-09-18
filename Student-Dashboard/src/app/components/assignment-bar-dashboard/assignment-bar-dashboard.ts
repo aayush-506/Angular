669d17f8144e5876a97b1e083595fd268e988b80
Aayush Chauhan <at290608@gmail.com>
2025-09-19 04:08:29 +0530

    fetching data from Assignment API

--- Student-Dashboard/src/app/components/assignment-bar-dashboard/assignment-bar-dashboard.ts
+++ Student-Dashboard/src/app/components/assignment-bar-dashboard/assignment-bar-dashboard.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
+import { Observable } from 'rxjs';
+import { Assignment } from '../../Interfaces/assignment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-assignment-bar-dashboard',
-  imports: [MatModule],
+  imports: [MatModule,CommonModule],
   templateUrl: './assignment-bar-dashboard.html',
   styleUrl: './assignment-bar-dashboard.scss'
 })
 export class AssignmentBarDashboard {
+@Input() assignment! : Assignment;
+today: Date = new Date();
+//finding remaining days
+getRemainingDays(dueDate: string | Date): number {
+    const due = new Date(dueDate).getTime(); // due date in ms
+    const now = new Date().getTime();        // current date in ms
+
+    const diff = due - now;                  // difference in ms
+    const remainingDays = Math.ceil(diff / (1000 * 60 * 60 * 24)); // convert to days
+
+    return remainingDays > 0 ? remainingDays : 0; // return 0 if past due
+  }
 
 }
