25515082eb74984fb5a93a73519698fb051037f2
Aayush Chauhan <at290608@gmail.com>
2025-09-18 16:43:37 +0530

    fetching data from API

--- Student-Dashboard/src/app/pages/attendence/attendence.ts
+++ Student-Dashboard/src/app/pages/attendence/attendence.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
 import { SmallCardDashboard } from '../../components/small-card-dashboard/small-card-dashboard';
+import { DashboardData } from '../../service/dashboard-data';
+import { map, Observable } from 'rxjs';
+import { Attendance } from '../../Interfaces/attendance';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-attendence',
-  imports: [MatModule,SmallCardDashboard],
+  standalone:true,
+  imports: [MatModule,SmallCardDashboard,CommonModule],
   templateUrl: './attendence.html',
   styleUrl: './attendence.scss'
 })
 export class Attendence {
 smallCard =
   {title:"Current Attendance",icon:"calendar_month",iconColor:"#9810FA",iconBg:"#FAF5FF"}
+attendance! : Observable<Attendance[]>
+attendancePercentage! : Observable<number>
 
+  constructor(private dashboardDataService : DashboardData){}
+ ngOnInit() {
+  this.dashboardDataService.loadAttendance();
+  this.attendance = this.dashboardDataService.getAttendanceData().pipe(
+    map(data => data.filter(a => a.studentId == 1))
+  );
+
+  this.attendancePercentage = this.attendance.pipe(map(data=>{
+    const totalAttended = data.reduce((sum,a)=>sum+a.attended,0);
+    const totalClasses = data.reduce((sum,a)=>sum+a.total,0);
+    return parseFloat(((totalAttended/totalClasses)*100).toFixed(2));
+  }))
+  
+}
 }
