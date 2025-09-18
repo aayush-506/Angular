9d11487bfa2245b7975facb381932727c6bd2e2f
Aayush Chauhan <at290608@gmail.com>
2025-09-19 04:58:36 +0530

    adding student's profile image

--- Student-Dashboard/src/app/components/navbar/navbar.ts
+++ Student-Dashboard/src/app/components/navbar/navbar.ts
@@ -3,10 +3,13 @@ import { MatModule } from '../../appModules/mat-module';
 import { DashboardData } from '../../service/dashboard-data';
 import { Searchbar } from '../searchbar/searchbar';
 import { NotificationIcon } from '../notification-icon/notification-icon';
+import { map, Observable } from 'rxjs';
+import { Student } from '../../Interfaces/student';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatModule,Searchbar,NotificationIcon],
+  imports: [MatModule,Searchbar,NotificationIcon,CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
@@ -14,14 +17,18 @@ export class Navbar {
 
   showProfile = true;
 
-  constructor(private dashboardData : DashboardData){}
+  constructor(private dashboardDataService : DashboardData){}
+
+  student! : Observable<Student[]>
+
   isSidenav(){
-    this.dashboardData.sidenavToggle();
+    this.dashboardDataService.sidenavToggle();
   }
   ngOnInit(){
-    this.dashboardData.sidenavState.subscribe({
+    this.dashboardDataService.sidenavState.subscribe({
       next : result => this.showProfile = result 
     })
+     this.student = this.dashboardDataService.getStudentData().pipe(map(data=>data.filter(a=>a.id == 1)));
   }
 
 }
