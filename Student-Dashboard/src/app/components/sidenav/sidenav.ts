7c7570b721f2b2b1264eb7e3e96b563be61399ea
Aayush Chauhan <at290608@gmail.com>
2025-09-19 04:47:55 +0530

    fetching student detail from student API

--- Student-Dashboard/src/app/components/sidenav/sidenav.ts
+++ Student-Dashboard/src/app/components/sidenav/sidenav.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { MatModule } from '../../appModules/mat-module';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { DashboardData } from '../../service/dashboard-data';
+import { map, Observable } from 'rxjs';
+import { Student } from '../../Interfaces/student';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [MatModule,RouterLink,RouterLinkActive],
+  imports: [MatModule,RouterLink,RouterLinkActive,CommonModule],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.scss'
 })
 export class Sidenav {
+
+constructor(private dashboardDataService : DashboardData ){}
+
+student! : Observable<Student[]>
+
+ngOnInit(){
+ this.student = this.dashboardDataService.getStudentData().pipe(map(data=>data.filter(a=>a.id == 1)));
+}
 
 }
