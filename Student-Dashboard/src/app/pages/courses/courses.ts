761be36f105ad484442a51c7ea3b7f19bc29ce61
Aayush Chauhan <at290608@gmail.com>
2025-09-18 19:23:28 +0530

    fetching students details from student API

--- Student-Dashboard/src/app/pages/courses/courses.ts
+++ Student-Dashboard/src/app/pages/courses/courses.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink} from '@angular/router';
+import { DashboardData } from '../../service/dashboard-data';
+import { map, Observable } from 'rxjs';
+import { Student } from '../../Interfaces/student';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './courses.html',
   styleUrl: './courses.scss'
 })
 export class Courses {
+
+  student! : Observable<Student[]>
+
+  constructor(private dashboardDataService : DashboardData){}
+
+ ngOnInit(){ 
+ 
+  this.dashboardDataService.loadStudent();
+  this.student = this.dashboardDataService.getStudentData().pipe(map(data=>data.filter(a=>a.id == 1)));
+
+ }
 
 }
