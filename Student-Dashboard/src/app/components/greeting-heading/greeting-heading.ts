09f7a8321511e1e7b228530283112976ee4bd916
Aayush Chauhan <at290608@gmail.com>
2025-09-19 05:06:05 +0530

    added student's name and current date

--- Student-Dashboard/src/app/components/greeting-heading/greeting-heading.ts
+++ Student-Dashboard/src/app/components/greeting-heading/greeting-heading.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DashboardData } from '../../service/dashboard-data';
+import { map, Observable } from 'rxjs';
+import { Student } from '../../Interfaces/student';
 
 @Component({
   selector: 'app-greeting-heading',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './greeting-heading.html',
   styleUrl: './greeting-heading.scss'
 })
 export class GreetingHeading {
+  today : Date = new Date ();
 
+  constructor(private dashboardDataService : DashboardData){}
+
+  student! : Observable<Student[]>
+
+  ngOnInit(){
+   this.student = this.dashboardDataService.getStudentData().pipe(map(data=>data.filter(a=>a.id == 1)));
+  }
 }
