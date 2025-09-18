a7facc979659ede8c3b88b4014c02d93549decdf
Aayush Chauhan <at290608@gmail.com>
2025-09-18 14:15:01 +0530

    added grades interface

---
+++ Student-Dashboard/src/app/Interfaces/grade.ts
@@ -0,0 +1,9 @@
+export interface Grade {
+    id : number;
+    studentId : number ;
+    courseName : string ;
+    midterm : number;
+    final : number;
+    assignments : number ;
+    grade : string ;
+}
