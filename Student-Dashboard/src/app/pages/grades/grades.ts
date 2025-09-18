fe5686cf6c7e464e78c12a5abdfd91c5b5ee3fc6
Aayush Chauhan <at290608@gmail.com>
2025-09-19 00:55:20 +0530

    fetching currentGPA from service

--- Student-Dashboard/src/app/pages/grades/grades.ts
+++ Student-Dashboard/src/app/pages/grades/grades.ts
@@ -15,19 +15,6 @@ import { CommonModule } from '@angular/common';
 })
 export class Grades {
 
-  gradePoints: Record<string, number> = {
-  "A+": 4.0,
-  "A": 4.0,
-  "A-": 3.7,
-  "B+": 3.3,
-  "B": 3.0,
-  "B-": 2.7,
-  "C+": 2.3,
-  "C": 2.0,
-  "D": 1.0,
-  "F": 0.0
-};
-
 smallCard =  {title:"Current GPA",icon:"school",iconColor:"#00A63E",iconBg:"#F0FDF4"}
 grades! : Observable<Grade[]> ;
 currentGPA!: Observable<number>;
@@ -36,20 +23,14 @@ constructor(private dashboardDataService : DashboardData){}
 
 ngOnInit(){
   this.dashboardDataService.loadGrades();
+  this.grades = this.dashboardDataService.getGradesData();
   const allGrades = this.dashboardDataService.getGradesData();
     this.grades = allGrades.pipe(
           map(data => data.filter(a => a.studentId === 1))
         );        
 
-    // calculating current CGPA
-    this.currentGPA = this.grades.pipe(
-      map(grades => {
-        const totalPoints = grades.reduce((sum, g) => {
-        return sum + (this.gradePoints[g.grade] || 0);
-      }, 0);
-      return parseFloat((totalPoints / grades.length).toFixed(2));
-      })
-    )        
+this.currentGPA = this.dashboardDataService.getGpa();
+  
   }
 }
 
