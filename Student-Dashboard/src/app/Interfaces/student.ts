5b5e28309fde9f6e389f3582c5f606027adc08c5
Aayush Chauhan <at290608@gmail.com>
2025-09-18 19:23:51 +0530

    added student interface

---
+++ Student-Dashboard/src/app/Interfaces/student.ts
@@ -0,0 +1,10 @@
+export interface Student {
+    id : number ;
+    name : string ;
+    email : string ;
+    password : string ;
+    branch : string ;
+    semester : number;
+    year : number ;
+    rollNo : string ;
+}
