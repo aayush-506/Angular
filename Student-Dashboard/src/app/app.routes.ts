0512b58c648d75f1d432278c68061106d6ec5165
Aayush Chauhan <at290608@gmail.com>
2025-09-19 17:24:28 +0530

    update the route structure of the app'

--- Student-Dashboard/src/app/app.routes.ts
+++ Student-Dashboard/src/app/app.routes.ts
@@ -6,14 +6,30 @@ import { Grades } from './pages/grades/grades';
 import { Assignments } from './pages/assignments/assignments';
 import { Announcements } from './pages/announcements/announcements';
 import { CoursesDetails } from './pages/courses-details/courses-details';
+import { LoginStudent } from './pages/login-student/login-student';
+import { DashboardLayout } from './pages/dashboard-layout/dashboard-layout';
+import { AuthGuard } from './service/auth-guard';
 
 
 export const routes: Routes = [
-{path:"" , component:MainDashboard},
-{path:"courses" , component:Courses},
-{path:"attendance" , component:Attendence},
-{path:"grades" , component:Grades},
-{path:"assignments" , component:Assignments},
-{path:"announcements" , component:Announcements},
-{path:"courses/:category",component:CoursesDetails}
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // default to login
+  { path: 'login', component: LoginStudent },
+
+  {
+    path: 'dashboard',
+    component: DashboardLayout, // wrapper with sidenav + navbar
+    canActivate: [AuthGuard],
+    canActivateChild :[AuthGuard],
+    children: [
+      { path: '', component: MainDashboard }, // default page inside dashboard
+      { path: 'courses', component: Courses },
+      { path: 'attendance', component: Attendence },
+      { path: 'grades', component: Grades },
+      { path: 'assignments', component: Assignments },
+      { path: 'announcements', component: Announcements },
+      { path: 'courses/:category', component: CoursesDetails }
+    ]
+  },
+
+//   { path: '**', redirectTo: 'login' } // fallback
 ];
