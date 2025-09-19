cd762a92bfeb8283081f8fe427ab722f4d8d5c5f
Aayush Chauhan <at290608@gmail.com>
2025-09-20 00:06:47 +0530

    added snackbar to the download btn

--- Student-Dashboard/src/app/pages/announcements/announcements.ts
+++ Student-Dashboard/src/app/pages/announcements/announcements.ts
@@ -2,6 +2,7 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 import { DashboardData } from '../../service/dashboard-data';
 import { CommonModule } from '@angular/common';
 import { Announcement } from '../../Interfaces/announcement';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-announcements',
@@ -12,7 +13,7 @@ import { Announcement } from '../../Interfaces/announcement';
 export class Announcements {
 
 announcements : Announcement[] = [];
-constructor(private dashboardDataService : DashboardData , private cdr : ChangeDetectorRef){}
+constructor(private dashboardDataService : DashboardData , private cdr : ChangeDetectorRef , private snackBar: MatSnackBar){}
 
 ngOnInit(){
   this.dashboardDataService.loadAnnouncements();
@@ -22,6 +23,21 @@ ngOnInit(){
      this.cdr.detectChanges();
   })
 }
+
+// snackbar
+download() {
+    const fileAvailable = false;
+    if (!fileAvailable) {
+      this.snackBar.open('No file available for download.', 'Close', {
+        duration: 3000,        
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+        panelClass: ['bg-red-600', 'text-white'] 
+      });
+    } else {
+      // your normal download logic here
+    }
+  }
 
 
 }
